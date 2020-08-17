b84207dec55a8646b2a8b530838f532c3e2fb1ce
Andrés Cimadamore <andrescimadamore@gmail.com>
2020-08-16 21:50:33 -0300

    Add subcriteria to Criterion model

--- models/Criterion.js
+++ models/Criterion.js
@@ -1,13 +1,54 @@
 // import _ from 'lodash'
 
 class Criterion {
-  constructor(name) {
+  constructor(name, subcriteria = []) {
     this.name = name
     this.weight = 0
+    this.parent = null
+    this.subcriteria = []
+
+    this._addSubcriteria(subcriteria)
   }
 
   toString() {
     return this.name
+  }
+
+  addSubcriterion(criterion) {
+    const idx = this.subcriteria.findIndex((c) => c.name === criterion.name)
+
+    if (idx === -1) {
+      criterion.parent = this
+      this.subcriteria.push(criterion)
+    }
+  }
+
+  // OPTIMIZE see if this interface makes sense. Change it to removeSubcriterionByName maybe?
+  removeSubcriterion(criterionName) {
+    const idx = this.subcriteria.findIndex((c) => c.name === criterionName)
+
+    if (idx !== -1) {
+      this.subcriteria.splice(idx, 1)
+    }
+  }
+
+  relativeWeight() {
+    return this.weight
+  }
+
+  // TODO cambiar esta regla de lintin de mierda
+  absoluteWeight() {
+    if (this.parent != null) {
+      return this.weight * this.parent.weight
+    } else {
+      return this.weight
+    }
+  }
+
+  _addSubcriteria(criteria) {
+    for (const criterion of criteria) {
+      this.addSubcriterion(criterion)
+    }
   }
 }
 
