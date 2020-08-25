4273e4a5ac0ec6ed4b80052e3c451efd733700dd
Andrés Cimadamore <andrescimadamore@gmail.com>
2020-08-25 17:04:22 -0300

    Show Criteria on profile details

---
+++ components/Criteria.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="list-none">
+    <Criterion
+      v-for="(criterion, key) in criteria"
+      :key="key"
+      :criterion="criterion"
+    >
+    </Criterion>
+  </ul>
+</template>
+
+<script>
+import CriterionComponent from '@/components/Criterion'
+
+const c = {
+  name: 'Criteria',
+  props: {
+    criteria: Array,
+  },
+}
+
+CriterionComponent.components = { Criteria: c }
+
+c.components = { Criterion: CriterionComponent }
+
+export default c
+</script>
---
+++ components/Criterion.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ml-8">
+    <li class="text-lg">
+      {{ criterion.name }} ({{ criterion.absoluteWeight() }}%)
+    </li>
+
+    <Criteria :criteria="criterion.subcriteria"></Criteria>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Criterion',
+  props: {
+    criterion: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
