30e50b8502cf990492e2164b808e0ccc5c364d1b
Andrés Cimadamore <andrescimadamore@gmail.com>
2020-08-24 22:21:54 -0300

    Add server requests to vuex store

--- store/index.js
+++ store/index.js
@@ -1,27 +1,86 @@
+import Profile from '@/models/Profile'
+import Axios from 'axios'
+
 export const state = () => ({
   profiles: [],
 })
 
 export const getters = {
-  getProfileByName: (state) => (name) => {
-    return state.profiles.find((p) => p.name === name)
+  getProfileById: (state) => (id) => {
+    return state.profiles.find((p) => p._id === id)
   },
 }
 
 export const mutations = {
+  setProfiles(state, profiles) {
+    state.profiles = profiles
+  },
+
   addProfile(state, profile) {
     state.profiles.push(profile)
   },
 
-  updateProfile(state, payload) {
-    const idx = state.profiles.findIndex((p) => p.name === payload.originalName)
+  updateProfile(state, profile) {
+    const idx = state.profiles.findIndex((p) => p._id === profile._id)
 
-    state.profiles[idx] = payload.profile
+    state.profiles[idx] = profile
   },
 
-  removeProfile(state, profileName) {
-    const idx = state.profiles.findIndex((p) => p.name === profileName)
+  removeProfile(state, profileId) {
+    const idx = state.profiles.findIndex((p) => p._id === profileId)
 
     state.profiles.splice(idx, 1)
+  },
+}
+
+export const actions = {
+  async fetchProfiles(context, payload) {
+    const { data } = await Axios.get('http://localhost:9000/profiles')
+    context.commit(
+      'setProfiles',
+      data.map((d) => Profile.deserealize(d))
+    )
+  },
+
+  async addProfile(context, payload) {
+    const { data } = await Axios({
+      method: 'post',
+      url: 'http://localhost:9000/profiles',
+      data: payload,
+      transformRequest: [
+        function (data, headers) {
+          headers['Content-Type'] = 'application/json'
+          return JSON.stringify(data, (k, v) =>
+            k === 'parent' ? undefined : v
+          )
+        },
+      ],
+    })
+
+    context.commit('addProfile', Profile.deserealize(data))
+  },
+
+  async updateProfile(context, profile) {
+    await Axios({
+      method: 'put',
+      url: `http://localhost:9000/profiles/${profile._id}`,
+      data: profile,
+      transformRequest: [
+        function (data, headers) {
+          headers['Content-Type'] = 'application/json'
+          return JSON.stringify(data, (k, v) =>
+            k === 'parent' ? undefined : v
+          )
+        },
+      ],
+    })
+
+    context.commit('updateProfile', profile)
+  },
+
+  async removeProfile(context, profileId) {
+    await Axios.delete(`http://localhost:9000/profiles/${profileId}`)
+
+    context.commit('removeProfile', profileId)
   },
 }
