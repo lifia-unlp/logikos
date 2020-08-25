29c80b56d6862a405ed69da18463ccdea0bab0a5
Andrés Cimadamore <andrescimadamore@gmail.com>
2020-08-25 20:30:30 -0300

    Add fetch alternative on frontend index

---
+++ components/Phone.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex w-1/3 m-2 p-2 border-2 rounded border-blue-400">
+    <img class="w-12" :src="phone.image" />
+    <p class="self-center">
+      <a :href="phone.url" target="_blank">{{ phone.model }}</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Phone',
+  props: {
+    phone: {
+      type: Object,
+    },
+  },
+}
+</script>
