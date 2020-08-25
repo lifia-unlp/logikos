beb0d5dc57eaba04c329063a8dd0d79604f7833d
Andrés Cimadamore <andrescimadamore@gmail.com>
2020-08-25 16:14:57 -0300

    Add frontend profile index and show

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,58 @@
 <template>
   <div class="container">
+    <h1 class="text-4xl">Neo-logikos</h1>
+
+    <label class="block font-bold">Profiles</label>
+    <select v-model="selectedProfileId">
+      <option v-for="(profile, i) in profiles" :key="i" :value="profile._id">
+        {{ profile.name }}
+      </option>
+    </select>
+
+    <div v-if="selectedProfileId">
+      <p>
+        Perfil seleccionado: {{ currentProfile.name }}
+        <nuxt-link
+          class="cursor-default"
+          :to="{
+            name: 'profiles-id',
+            params: { id: currentProfile._id },
+          }"
+        >
+          (ver)
+        </nuxt-link>
+      </p>
+
+      <ul>
+        <li
+          v-for="(criterion, i) in currentProfile.getComparableCriteria()"
+          :key="i"
+          class="inline p-1 m-1 bg-blue-400 rounded text-white font-bold"
+        >
+          {{ criterion.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      selectedProfileId: null,
+    }
+  },
+  computed: {
+    ...mapState(['profiles']),
+    currentProfile() {
+      return this.$store.getters.getProfileById(this.selectedProfileId)
+    },
+  },
+  created() {
+    this.$store.dispatch('fetchProfiles')
+  },
+}
 </script>
---
+++ pages/profiles/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-4xl">{{ profile.name }}</h1>
+</template>
+
+<script>
+export default {
+  computed: {
+    profile() {
+      return this.$store.getters.getProfileById(this.$route.params.id)
+    },
+  },
+}
+</script>
